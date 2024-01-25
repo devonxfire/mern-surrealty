97ab4be04166e9ee2d9fd3a1768f2c0e2d1e6473
devonmartindale <devonmartindale@gmail.com>
2024-01-25 16:45:28 +0200

    created mongoDB database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { connectDB } from "./config/connectDB.js";
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
 app.listen(3000, () => console.log("Server listening on port 3000..."));
+
+//devon
+//devon123
