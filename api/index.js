5d8d918ac2a4147a28450acfd5e4bd96d26655df
devonmartindale <devonmartindale@gmail.com>
2024-01-25 17:12:01 +0200

    setup test api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { connectDB } from "./config/connectDB.js";
+import userRouter from "./routes/user.route.js";
 
 // Connect to MongoDB
 connectDB();
 
 const app = express();
+
+// Routes
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => console.log("Server listening on port 3000..."));
 
