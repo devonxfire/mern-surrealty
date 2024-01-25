043aa996abaa03209650e5ffd3afc52e06394d6d
devonmartindale <devonmartindale@gmail.com>
2024-01-25 15:25:22 +0200

    setup pages and routes on client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signout from "./pages/Signout";
+import Profile from "./pages/Profile";
+
 export default function App() {
-  return <h1 className="text-red-700">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signout" element={<Signout />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
