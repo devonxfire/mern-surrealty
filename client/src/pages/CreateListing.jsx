0d9e2e531933ee6ad98c91017d3117ce4f781fc1
devonmartindale <devonmartindale@gmail.com>
2024-01-30 16:58:00 +0200

    complete upload listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,86 @@
-import React from "react";
+import { useState } from "react";
+import { MdDelete } from "react-icons/md";
+// Firebase Image Imports
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+//
 
 const CreateListing = () => {
+  const [files, setFiles] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  const [formData, setFormdata] = useState({
+    imageUrls: [],
+  });
+
+  console.log(formData);
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleImageUpload = () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormdata({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError(err.message);
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("Please select between 1 and 6 images");
+      setUploading(false);
+    }
+  };
+
+  const handleDeleteImage = (index) => {
+    setFormdata({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
+
   return (
     <main className="pt-8 p-3 max-w-4xl mx-auto min-h-screen">
       <h1 className="text-center text-orange-500 font-extrabold text-xl sm:text-3xl pb-8">
@@ -121,11 +201,42 @@ const CreateListing = () => {
               accept="image/*"
               multiple
               className="p-3  focus:outline-none w-full "
+              onChange={(e) => setFiles(e.target.files)}
             />
-            <button className="p-3 bg-purple-500 text-white uppercase rounded-lg hover:opacity-90 disabled:opacity-75">
-              upload
+            <button
+              className="p-3 bg-purple-500 text-white uppercase rounded-lg hover:opacity-90 disabled:opacity-75"
+              type="button"
+              disabled={uploading}
+              onClick={handleImageUpload}
+            >
+              {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
+          <p className="text-red-500 text-xs">
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                className="flex justify-between p-3 border rounded-lg items-center"
+                key={url}
+              >
+                <img
+                  src={url}
+                  alt="listing"
+                  className="w-20 h-20 object-contain  overflow-hidden"
+                />
+                <div
+                  className="flex items-center text-red-500 gap-1"
+                  onClick={() => handleDeleteImage(index)}
+                >
+                  <button className=" text-red-500 uppercase hover:opacity-90">
+                    delete
+                  </button>
+                  <MdDelete className="text-lg cursor-pointer" />
+                </div>
+              </div>
+            ))}
           <button className="p-3 uppercase text-white  rounded-lg  bg-lime-500 w-full hover:opacity-90 disabled:op-70">
             Create Listing
           </button>
@@ -136,3 +247,14 @@ const CreateListing = () => {
 };
 
 export default CreateListing;
+
+// service firebase.storage {
+//     match /b/{bucket}/o {
+//       match /{allPaths=**} {
+//         allow read;
+//         allow write: if
+//         request.resource.size < 10 * 1024 * 1024 &&
+//         request.resource.contentType.matches('image/.*')
+//       }
+//     }
+//   }
