85b097014c3e4569955b42bd462cda1dc23bcda6
devonmartindale <devonmartindale@gmail.com>
2024-02-08 08:10:35 +0200

    added hamburger menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { useSelector } from "react-redux";
 import { BiSolidHomeHeart } from "react-icons/bi";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [searchTerm, setsearchTerm] = useState("");
+  const [burgerClick, setBurgerClick] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -24,6 +26,11 @@ const Header = () => {
       setsearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
+
+  const handleBurgerClick = () => {
+    setBurgerClick(!burgerClick);
+    console.log("burger clicked!");
+  };
   return (
     <header className="  bg-slate-100 ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
@@ -31,7 +38,7 @@ const Header = () => {
           to="/"
           className="flex items-center text-md sm:text-lg md:text-3xl text-red-600"
         >
-          <BiSolidHomeHeart className="text-md sm:text-2xl md:text-3xl lg:text-4xl" />
+          <BiSolidHomeHeart className="text-md sm:text-2xl md:text-2xl lg:text-3xl" />
           <h1 className="text-xl sm:text-3xl lg:text-4xl flex ">
             <span
               className="
@@ -39,7 +46,7 @@ const Header = () => {
             >
               Surrealty
             </span>
-            <span className="text-red-600 uppercase font-extrabold ">
+            <span className="text-slate-400 uppercase font-extralight">
               Estates
             </span>
           </h1>
@@ -57,19 +64,57 @@ const Header = () => {
             value={searchTerm}
           />
           <FaSearch
-            className="text-slate-500 cursor-pointer "
+            className="text-slate-500 cursor-pointer hidden lg:block"
             onClick={handleSubmit}
           />
         </form>
         <nav>
           <ul className="flex items-center gap-4 text-slate-500 text-xs lg:text-base font-light">
+            <div className="relative">
+              <RxHamburgerMenu
+                className="md:hidden block text-lg hover:cursor-pointer"
+                onClick={handleBurgerClick}
+              />
+              {burgerClick && (
+                <div className="flex flex-col absolute md:hidden bg-white shadow-lg p-2 mt-1 pb-4">
+                  <ul className="flex flex-col gap-2 mt-2 whitespace-nowrap">
+                    <Link to="/">
+                      <li className="hover:underline hover:text-red-600">
+                        Home
+                      </li>
+                    </Link>
+                    <Link to="/about">
+                      <li className="hover:underline hover:text-red-600">
+                        About
+                      </li>
+                    </Link>
+                    {currentUser ? (
+                      <Link to="/profile">
+                        <img
+                          src={currentUser.photo}
+                          alt="user-photo"
+                          className="w-8 h-8 sm:w-12 sm:h-12 rounded-full object-cover "
+                        />
+                      </Link>
+                    ) : (
+                      <Link to="/signin">
+                        <li className="  hover:underline hover:text-red-600">
+                          Sign In
+                        </li>
+                      </Link>
+                    )}
+                  </ul>
+                </div>
+              )}
+            </div>
+
             <Link to="/">
-              <li className="hidden sm:inline hover:underline hover:text-red-600">
+              <li className="hidden md:inline hover:underline hover:text-red-600">
                 Home
               </li>
             </Link>
             <Link to="/about">
-              <li className="hidden sm:inline hover:underline hover:text-red-600">
+              <li className="hidden md:inline hover:underline hover:text-red-600">
                 About
               </li>
             </Link>
@@ -78,12 +123,12 @@ const Header = () => {
                 <img
                   src={currentUser.photo}
                   alt="user-photo"
-                  className="w-8 h-8 sm:w-12 sm:h-12 rounded-full object-cover "
+                  className="hidden md:block w-8 h-8 sm:w-12 sm:h-12 rounded-full object-cover "
                 />
               </Link>
             ) : (
               <Link to="/signin">
-                <li className="  hover:underline hover:text-red-600">
+                <li className="hidden md:block  hover:underline hover:text-red-600">
                   Sign In
                 </li>
               </Link>
